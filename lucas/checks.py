fa9529316d5122d535265ef21f4cb144da4e4475
Ana Paula Gomes <apgomes88@gmail.com>
2023-04-17 18:21:18 +0200

    [WIP] Checa se tem recaptcha ou não

--- lucas/checks.py
+++ lucas/checks.py
@@ -1,15 +1,52 @@
 import requests
 
 
+def _has_recaptcha(html):  # TODO add typing
+    """Check if recaptcha is on.
+
+    Docs: https://developers.google.com/recaptcha/docs/v3
+    """
+    possibilities = [
+        html.find("iframe[title^='reCAPTCHA']"),
+        html.find("input#recaptcha-token"),
+        html.find("div.g-recaptcha"),
+        html.find("textarea#g-recaptcha-response"),
+        html.find("textarea.g-recaptcha-response"),
+        html.find("script[src*='www.google.com/recaptcha']"),
+    ]
+    return any(possibilities)
+
+
+def has_robot_blocker(body):
+    """Verify if robot blockers, like recaptcha, are configured."""
+    return _has_recaptcha(body)
+
+
 def is_reachable(url):
-    response = requests.get(url)
+    result = {
+        "reachable": True,
+        "robot_friendly": True,
+    }
+    response = requests.get(url, timeout=(3.05, 27))
     try:
         response.raise_for_status()
     except requests.RequestException:
-        return False
-    return True
+        result["reachable"] = False
+    else:
+        if has_robot_blocker(response.text):
+            result["robot_friendly"] = False
+    return result
 
 
-if __name__ == "__main__":
-    url = "http://www.transparencia.feiradesantana.ba.gov.br/"
-    print(f"Acessível? {'✅' if is_reachable(url) else '❌'}")
+url = "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=contratos"
+
+
+def request_html_from(url):
+    from requests_html import HTMLSession
+
+    session = HTMLSession()
+    response = session.get(url)
+    # response.html.render() # TimeoutError: Navigation Timeout Exceeded: 8000 ms exceeded.
+    # response.html.render(wait=10, sleep=15) # TimeoutError: Navigation Timeout Exceeded: 8000 ms exceeded.
+    # response.html.render(30) # TimeoutError: Navigation Timeout Exceeded: 8000 ms exceeded.
+    response.html.render(timeout=30)
--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 import pytest
 
-from lucas.checks import is_reachable
+from lucas.checks import has_robot_blocker, is_reachable
 
 
 @pytest.mark.parametrize(
@@ -16,4 +18,9 @@ def test_is_reachable(status_code, expected_result, requests_mock):
     fake_url = "https://www.fake.com.br"
     requests_mock.get(fake_url, status_code=status_code)
 
-    assert is_reachable(fake_url) is expected_result
+    assert is_reachable(fake_url)["reachable"] is expected_result
+
+
+def test_has_robot_blocker():
+    body = Path("tests/fixtures/contratos.html").read_text()
+    assert has_robot_blocker(body) is True

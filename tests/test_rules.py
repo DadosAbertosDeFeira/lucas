75d8546a581df8c12163ee22a83459fa88a6775f
Ana Paula Gomes <apgomes88@gmail.com>
2023-04-29 14:54:37 +0200

    WIP

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,54 +1,38 @@
+import re
+
 import pytest
 from playwright.sync_api import expect
 
+pages = {
+    "portal": "http://www.transparencia.feiradesantana.ba.gov.br/",
+    "despesas": "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=despesa",
+    "receitas": "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=receita",
+    "licitacoes": "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=licitacoes",
+    "contratos": "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=contratos",
+}
+
 
 @pytest.mark.rules
-def test_page_show_contracts(page):
-    page.goto(
-        "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=contratos"
-    )
+@pytest.mark.parametrize("url", pages.values(), ids=pages.keys())
+def test_page_is_accessible_by_humans(page, url):
+    response = page.request.get(url)
+    expect(response).to_be_ok()
+
+    page.goto(url)
     page.wait_for_load_state()
 
-    expect(page.get_by_text("Nº CONTRATO")).to_be_visible()
-    expect(page.get_by_text("SECRETARIA")).to_be_visible()
-    expect(page.get_by_text("FORNECEDOR")).to_be_visible()
-    expect(page.get_by_text("DATA ASSINATURA")).to_be_visible()
-    expect(page.get_by_text("INÍCIO EXECUÇÃO")).to_be_visible()
-    expect(page.get_by_text("FIM EXECUÇÃO")).to_be_visible()
-    expect(page.get_by_text("VALOR")).to_be_visible()
+    expect(page).to_have_title(re.compile("Portal da Transparência"))
 
 
 @pytest.mark.rules
-def test_page_show_revenue(page):
-    page.goto(
-        "http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=receita"
-    )
-    page.wait_for_load_state()
-    page.get_by_role("button", name="Pesquisar").click()
-
-    published_date_text = page.locator(
-        '//*[@id="editable-sample"]/tbody/tr[1]/td[1]'
-    ).text_content()
-    # TODO check if it is a valid date
-
-    expect(page.get_by_text("Data de Publicação")).to_be_visible()
-    expect(page.get_by_text("Valor")).to_be_visible()
-
-
-@pytest.mark.rules
-@pytest.mark.parametrize(
-    "url",
-    ["http://www.transparencia.feiradesantana.ba.gov.br/index.php?view=contratos"],
-)
+@pytest.mark.parametrize("url", pages.values(), ids=pages.keys())
 def test_page_is_accessible_by_robots(page, url):
     page.goto(url)
     page.wait_for_load_state()
 
-    outter_iframe = page.frame_locator("iframe")
-    recaptcha_iframe = outter_iframe.locator('iframe[title^="reCAPTCHA"]')
-    textarea = outter_iframe.locator("textarea")
+    outer_iframe = page.frame_locator("iframe")
+    recaptcha_iframe = outer_iframe.locator('iframe[title^="reCAPTCHA"]')
+    textarea = outer_iframe.locator("textarea=g-recaptcha-response")
 
     expect(recaptcha_iframe, "should not have recaptcha").not_to_be_visible()
-    expect(textarea, "should not have g-recaptcha-response").not_to_have_id(
-        "g-recaptcha-response"
-    )
+    expect(textarea, "should not have g-recaptcha-response").to_have_count(0)
